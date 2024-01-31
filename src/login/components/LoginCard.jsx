7b16999d9f161e66d4a1aefd3ced244ddea3d746
hhuseyn1 <huseyn.hemidov.2004@gmail.com>
2024-01-31 12:51:16 +0400

    config tailwind

---
+++ src/login/components/LoginCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function LoginCard() {
+  return (
+    <>
+      <div className='flex h-screen justify-center items-center'>
+          <h1>Hello Again!</h1>
+          <h2>Welcome Back</h2>
+      </div>
+    </>
+  )
+}
