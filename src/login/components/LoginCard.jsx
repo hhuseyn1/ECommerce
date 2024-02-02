41c811db4e7ccd769ad747fe499de1a250ea8445
hhuseyn1 <huseyn.hemidov.2004@gmail.com>
2024-02-02 18:18:38 +0400

    fix login responsive

--- src/login/components/LoginCard.jsx
+++ src/login/components/LoginCard.jsx
@@ -11,7 +11,7 @@ export default function LoginCard() {
           <h1 className='font-bold text-5xl'>Hello Again!</h1>
           <h4 className='text-2xl'>Welcome Back</h4>
         </div>
-        <div className='flex flex-col mt-10 relative min-[320px]:w-full'>
+        <div className='flex flex-col mt-10 relative px-5 sm:px-0 min-[320px]:w-full'>
           <div className='relative mb-6'>
             <img src={MailIcon} className='w-6 h-6 absolute left-3 top-1/2 transform -translate-y-1/2 z-10' alt='Mail Icon' />
             <input type="email" placeholder='Email address' className='w-full sm:w-[388px] h-[48px] pl-10 rounded-full border border-gray-300 focus:outline-none focus:border-[#5ECE7B]' />
